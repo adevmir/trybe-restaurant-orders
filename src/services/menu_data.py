5688c1bced07a6071cf6bf1aee739c300d3a8756
adevmir <ademirvalonqar@gmail.com>
2023-04-25 13:20:01 -0300

    [Req 3] - Implementa a classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,20 @@
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path) as file:
+            csv_file = csv.DictReader(file)
+            menu = list(csv_file)
+        dishes = {}
+
+        for item in menu:
+            if item["dish"] not in dishes:
+                dishes[item['dish']] = Dish(item['dish'], float(item['price']))
+            ingredient = Ingredient(item['ingredient'])
+            amount = float(item['recipe_amount'])
+            dishes[item['dish']].add_ingredient_dependency(ingredient, amount)
+        self.dishes = set(dishes.values())
