3089549504ff84d89935b624525de342b4be3a3e
adevmir <ademirvalonqar@gmail.com>
2023-04-25 11:52:38 -0300

    correcoes de arquivos

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,26 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction
+)  # noqa: F401, E261, E501
+
+
+INGREDIENTS = (
+    Ingredient("queijo mussarela"),
+    Ingredient("tomate"),
+)
 
 
 # Req 1
 def test_ingredient():
-    pass
+    assert INGREDIENTS[0].restrictions == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.LACTOSE,
+        }
+    assert repr(INGREDIENTS[0]) == "Ingredient('queijo mussarela')"
+    assert repr(INGREDIENTS[0]) != "Ingredient('tomate')"
+    assert INGREDIENTS[0] == Ingredient("queijo mussarela")
+    assert INGREDIENTS[1] == Ingredient("tomate")
+    assert INGREDIENTS[0] != INGREDIENTS[1]
+    assert hash(INGREDIENTS[0]) == hash(Ingredient("queijo mussarela"))
+    assert hash(INGREDIENTS[1]) == hash(Ingredient("tomate"))
+    assert INGREDIENTS[0].name == "queijo mussarela"
